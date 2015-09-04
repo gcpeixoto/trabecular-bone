ec89dfab42a15923ad780dd3dbec3e56bfc270e3
Gustavo PEIXOTO DE OLIVEIRA <tavolesliv@gmail.com>
2015-09-04 13:58:42 -0300

    Correcting function px2m.

--- lib/px2m.m
+++ lib/px2m.m
@@ -1,23 +1,27 @@
-function y = px2m( x, n)
+function y = px2m( x, n, varargin)
 % px2m Convert from px to meter
 %
 %   input:
-%       y: number in meters
+%       y: number in pixels
 %       n: dimension
 %
 %   output
-%       x: number in pixels
+%       x: number in meters
 
-% 1 voxel = 34 micrometers; then 1 px = cubic root of 34
-px = (34*1e-6)^(1/3);
+% 1 px = 34 micrometers; Roque (2013)
+m = (34*1e-6);
 
-switch n 
-    case 1
-        y = x*px;      % value in m
-    case 2
-        y = x*(px)^2;  % value in m^2 (area)
-    case 3
-        y = x*(px)^3;  % value in m^3 (volume) 
+if nargin == 1
+    y = x*m;
+else
+    switch n 
+        case 1
+            y = x*m;      % value in m
+        case 2
+            y = x*m^2;  % value in m^2 (area)
+        case 3
+            y = x*m^3;  % value in m^3 (volume) 
+    end
 end
 
 end
