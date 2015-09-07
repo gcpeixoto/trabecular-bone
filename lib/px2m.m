fbb023519173d233031da2915f7a916188e94a25
Gustavo PEIXOTO DE OLIVEIRA <tavolesliv@gmail.com>
2015-09-06 22:37:24 -0300

    Improving comments for px2m.m

--- lib/px2m.m
+++ lib/px2m.m
@@ -1,22 +1,26 @@
-function y = px2m( x, n, varargin)
+function y = px2m( x, n, varargin )
 % px2m Convert from px to meter
 %
+%   If n = 1, x must be a value in px   (line)
+%   If n = 2, x must be a value in px^2 (area)
+%   If n = 3, x must be a value in px^3 (volume)
+%
 %   input:
-%       y: number in pixels
+%       x: number in pixels
 %       n: dimension
 %
 %   output
-%       x: number in meters
+%       y: number in meters
 
-% 1 px = 34 micrometers; Roque (2013)
-m = (34*1e-6);
+% assumed conversion: 1 px = 34 micrometers; Roque (2013)
+m = (34.0*1e-6);
 
-if nargin == 1
+if nargin == 1 
     y = x*m;
 else
     switch n 
         case 1
-            y = x*m;      % value in m
+            y = x*m;    % value in m   (line)
         case 2
             y = x*m^2;  % value in m^2 (area)
         case 3
